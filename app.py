bc94cc37e488ac0675c3989e35c033a53252043c
Gail Kicks <gailkicks@aol.com>
2024-04-06 15:33:06 +0000

    create project_summary.py and app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+
+# load pages scripts
+from app_pages.project_summary import page_summary_body
+# from app_pages.page_churned_customer_study import page_churned_customer_study_body
+
+app = MultiPage(app_name= "Predicting Housing Prices in Ames, Iowa") # Create an instance of the app 
+
+# Add your app pages here using .add_page()
+app.add_page("Project Summary", page_summary_body)
+# app.add_page("Customer Base Churn Study", page_churned_customer_study_body)
+
+app.run() # Run the  app
