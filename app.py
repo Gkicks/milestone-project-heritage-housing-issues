53ef8b8e5d484ef54f37d72be9383cfeae627361
Gkicks <gailkicks@aol.com>
2024-04-23 18:58:37 +0000

    create app pages

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 # load pages scripts
-from app_pages.project_summary import page_summary_body
+from app_pages.p1_project_summary import page_summary_body
 # from app_pages.page_churned_customer_study import page_churned_customer_study_body
 
 app = MultiPage(app_name= "Predicting Housing Prices in Ames, Iowa") # Create an instance of the app 
