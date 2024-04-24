7c8c0f9226645430e457a8bfe7f3bdfd21165279
Gkicks <gailkicks@aol.com>
2024-04-24 22:29:29 +0000

    complete evaluate.py and p5_ml_predict_house_price

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from app_pages.p1_project_summary import page_summary_body
 from app_pages.p2_house_sale_price_study import page_sale_price_study_body
 from app_pages.p3_predict_house_price import page_predict_house_price_body
 from app_pages.p4_project_hypothesis_and_validation import page_project_hypothesis_body
+from app_pages.p5_ml_predict_house_price import page_ml_predict_house_price_body
 
 # Create an instance of the app
 app = MultiPage(app_name= "Predicting Housing Prices in Ames, Iowa")  
@@ -15,6 +16,6 @@ app.add_page("Project Summary", page_summary_body)
 app.add_page("House Sale Price Study", page_sale_price_study_body)
 app.add_page("Predict House Price", page_predict_house_price_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
-# app.add_page("ML - Predict House Price", )
+app.add_page("ML - Predict House Price", page_ml_predict_house_price_body)
 
 app.run() # Run the  app
---
+++ app_pages/p5_ml_predict_house_price.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from src.data_management import load_housing_data, load_pkl_file
+from src.machine_learning.evaluate import performance, evaluation, evaluation_plots
+
+
+def page_ml_predict_house_price_body():
+
+    version = 'v1'
+    # load needed files
+    sale_price_pipe_model = load_pkl_file(
+        f'outputs/ml_pipeline/predict_sale_price/{version}/pipeline.pkl')
+    sale_price_feat_importance = plt.imread(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/features_importance.png")
+    X_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_train.csv")
+    X_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/X_test.csv")
+    y_train = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_train.csv").values
+    y_test = pd.read_csv(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/y_test.csv").values
+
+    st.write("### ML Pipeline: Predict Sales Price")
+    # display pipeline training summary conclusions
+    st.info(
+        f"* The aim of the pipline was to achieve an R2 score of at 0.75 "
+        f"on the train set as well as on the test set"
+        f"* The pipeline performance on train and test set is 0.98 and 0.85, respectively."
+    )
+
+    # show pipelines
+    st.write("---")
+
+    st.write("* The The pipeline that was used in this project:")
+    st.write(sale_price_pipe_model)
+
+    # show feature importance plot
+    st.write("---")
+    st.write("* The features the model was trained and their importance.")
+    st.write(X_train.columns.to_list())
+    st.image(sale_price_feat_importance)
+
+
+    # evaluate performance on train and test set
+    st.write("---")
+    st.write("### Pipeline Performance")
+    performance(X_train, y_train, X_test, y_test, sale_price_pipe_model)
