ea3dd71f000d8fd748cef48591637c1994c93286
Gkicks <gailkicks@aol.com>
2024-04-27 15:38:21 +0000

    add performance plots to dashboard

--- app_pages/p5_ml_predict_house_price.py
+++ app_pages/p5_ml_predict_house_price.py
@@ -14,6 +14,9 @@ def page_ml_predict_house_price_body():
     sale_price_feat_importance = plt.imread(
         f"outputs/ml_pipeline/predict_sale_price/{version}/features_importance"
         f".png")
+    evaluation_plots = plt.imread(
+        f"outputs/ml_pipeline/predict_sale_price/{version}/"
+        f"regression_evaluation_plots.png")
     X_train = pd.read_csv(
         f"outputs/ml_pipeline/predict_sale_price/{version}/X_train.csv")
     X_test = pd.read_csv(
@@ -29,7 +32,10 @@ def page_ml_predict_house_price_body():
         f"* The aim of the pipline was to achieve an R2 score of at 0.75 "
         f"on the train set as well as on the test set\n"
         f"* The pipeline performance on train and test set is 0.98 and 0.83, "
-        f"respectively."
+        f"respectively\n"
+        f"* The performance plots show that the data points seem to fit the "
+        f"regession line well which suggests the model is neither overfitting "
+        f"nor underfitting."
     )
 
     # show pipelines
@@ -40,6 +46,7 @@ def page_ml_predict_house_price_body():
 
     # show feature importance plot
     st.write("---")
+    st.write("### Feature Importance Plot")
     st.write("* The features the model was trained and their importance.")
     st.write(X_train.columns.to_list())
     st.image(sale_price_feat_importance)
@@ -48,3 +55,6 @@ def page_ml_predict_house_price_body():
     st.write("---")
     st.write("### Pipeline Performance")
     performance(X_train, y_train, X_test, y_test, sale_price_pipe_model)
+    st.text("")
+    st.write("### Pipeline Performance Plots")
+    st.image(evaluation_plots)
