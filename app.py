95f7b73364c0ce6649bd0e3fc7e6a814abffa5a0
Gkicks <gailkicks@aol.com>
2024-04-28 14:19:01 +0000

    add comments and docstrings to functions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from app_pages.multipage import MultiPage
 
-# load pages scripts
+# Load pages scripts
 from app_pages.p1_project_summary import page_summary_body
 from app_pages.p2_house_sale_price_study import page_sale_price_study_body
 from app_pages.p3_predict_house_price import page_predict_house_price_body
@@ -20,4 +20,5 @@ app.add_page("Predict House Price", page_predict_house_price_body)
 app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
 app.add_page("ML - Predict House Price", page_ml_predict_house_price_body)
 
-app.run()  # Run the  app
+# Run the  app
+app.run()  
